b0cf3de76bdd698029d23f3ebdef33fea31bd41d
sixiaoyou <yunshaliying@sina.com>
2017-05-02 22:00:24 +0800

    Update

---
+++ src/main/webapp/scripts/convans/index.js
@@ -0,0 +1,10 @@
+/**
+ * Created by You on 2017/5/2.
+ */
+
+
+var canvas = document.getElementById("canvas");
+var context = canvas.getContext("2d");
+
+context.fillStyle="#ff0000";
+context.fillRect(0,0,100,100)
